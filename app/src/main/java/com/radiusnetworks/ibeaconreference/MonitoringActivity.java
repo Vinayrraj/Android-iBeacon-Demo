085aebc55cde8590f89ac94a5e3e94e06f6b98fc
Sergio Martinez <ser.mar.rod.smr@gmail.com>
2015-11-13 11:54:17 +0100

    Update AltBacon lib to 2.X version
    Update AltBacon lib to 2.X version

---
+++ app/src/main/java/com/radiusnetworks/ibeaconreference/MonitoringActivity.java
@@ -0,0 +1,193 @@
+package com.radiusnetworks.ibeaconreference;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+public class MonitoringActivity extends Activity implements BeaconConsumer {
+	protected static final String TAG = "MonitoringActivity";
+
+	private ListView list = null;
+	private BeaconAdapter adapter = null;
+	private ArrayList<Beacon> arrayL = new ArrayList<Beacon>();
+	private LayoutInflater inflater;
+
+	private BeaconServiceUtility beaconUtill = null;
+	private BeaconManager iBeaconManager = BeaconManager.getInstanceForApplication(this);
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_monitor);
+		beaconUtill = new BeaconServiceUtility(this);
+		list = (ListView) findViewById(R.id.list);
+		adapter = new BeaconAdapter();
+		list.setAdapter(adapter);
+		inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		beaconUtill.onStart(iBeaconManager, this);
+	}
+
+	@Override
+	protected void onStop() {
+		beaconUtill.onStop(iBeaconManager, this);
+		super.onStop();
+	}
+
+	@Override
+	public void onBeaconServiceConnect() {
+
+		iBeaconManager.setRangeNotifier(new RangeNotifier() {
+			@Override
+			public void didRangeBeaconsInRegion(Collection<Beacon> iBeacons, Region region) {
+
+				arrayL.clear();
+				arrayL.addAll((ArrayList<Beacon>) iBeacons);
+				runOnUiThread(new Runnable() {
+					@Override public void run() {
+						adapter.notifyDataSetChanged();
+					}
+				});
+			}
+
+		});
+
+		iBeaconManager.setMonitorNotifier(new MonitorNotifier() {
+			@Override
+			public void didEnterRegion(Region region) {
+				Log.e("BeaconDetactorService", "didEnterRegion");
+				// logStatus("I just saw an iBeacon for the first time!");
+			}
+
+			@Override
+			public void didExitRegion(Region region) {
+				Log.e("BeaconDetactorService", "didExitRegion");
+				// logStatus("I no longer see an iBeacon");
+			}
+
+			@Override
+			public void didDetermineStateForRegion(int state, Region region) {
+				Log.e("BeaconDetactorService", "didDetermineStateForRegion");
+				// logStatus("I have just switched from seeing/not seeing iBeacons: " + state);
+			}
+
+		});
+
+		try {
+			iBeaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			iBeaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private class BeaconAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			if (arrayL != null && arrayL.size() > 0)
+				return arrayL.size();
+			else
+				return 0;
+		}
+
+		@Override
+		public Beacon getItem(int arg0) {
+			return arrayL.get(arg0);
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			return arg0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			try {
+				ViewHolder holder;
+
+				if (convertView != null) {
+					holder = (ViewHolder) convertView.getTag();
+				} else {
+					holder = new ViewHolder(convertView = inflater.inflate(R.layout.tupple_monitoring, null));
+				}
+				if (arrayL.get(position).getId1() != null)
+					holder.beacon_uuid.setText("UUID: " + arrayL.get(position).getId1());
+
+				holder.beacon_major.setText("Major: " + arrayL.get(position).getId2());
+
+				holder.beacon_minor.setText(", Minor: " + arrayL.get(position).getId3());
+
+				holder.beacon_proximity.setText("Proximity: " + "N/A");
+
+				holder.beacon_rssi.setText(", Rssi: " + arrayL.get(position).getRssi());
+
+				holder.beacon_txpower.setText(", TxPower: " + arrayL.get(position).getTxPower());
+
+				holder.beacon_range.setText("" + arrayL.get(position).getDistance());
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return convertView;
+		}
+
+		private class ViewHolder {
+			private TextView beacon_uuid;
+			private TextView beacon_major;
+			private TextView beacon_minor;
+			private TextView beacon_proximity;
+			private TextView beacon_rssi;
+			private TextView beacon_txpower;
+			private TextView beacon_range;
+
+			public ViewHolder(View view) {
+				beacon_uuid = (TextView) view.findViewById(R.id.BEACON_uuid);
+				beacon_major = (TextView) view.findViewById(R.id.BEACON_major);
+				beacon_minor = (TextView) view.findViewById(R.id.BEACON_minor);
+				beacon_proximity = (TextView) view.findViewById(R.id.BEACON_proximity);
+				beacon_rssi = (TextView) view.findViewById(R.id.BEACON_rssi);
+				beacon_txpower = (TextView) view.findViewById(R.id.BEACON_txpower);
+				beacon_range = (TextView) view.findViewById(R.id.BEACON_range);
+
+				view.setTag(this);
+			}
+		}
+
+	}
+
+}
